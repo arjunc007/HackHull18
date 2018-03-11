9c21c7a726b32fb67eb6f0439058c7d0d03d5c64
arjunc007 <arjunchauhan007@gmail.com>
2018-03-11 13:27:22 +0000

    Added fake data for things

--- TodoApi/Controllers/UserController.cs
+++ TodoApi/Controllers/UserController.cs
@@ -18,16 +18,42 @@ namespace TodoApi.Controllers
         {
             _context = context;
 
+            string id = "";
             if (_context.UserItems.Count() == 0)
             {
                 var users = new List<UserViewModel>
                 {
-                    new UserViewModel { ID = Guid.NewGuid().ToString(), Password = "pass", Username = "Arjun"},
-                    new UserViewModel { ID = Guid.NewGuid().ToString(), Password = "password", Username = "Iskandar" },
-                    new UserViewModel { ID = Guid.NewGuid().ToString(), Password = "words", Username = "Josh" },
-                    new UserViewModel { ID = Guid.NewGuid().ToString(), Password = "", Username = "Ephra" }
+                    new UserViewModel { ID = "1000", Password = "pass", Username = "Arjun"},
+                    new UserViewModel { ID = "1001", Password = "password", Username = "Iskandar" },
+                    new UserViewModel { ID = "1002", Password = "words", Username = "Josh" },
+                    new UserViewModel { ID = "1003", Password = "", Username = "Ephra" }
                 };
+                id = users[0].ID;
                 _context.AddRange(users);
+            }
+
+            if(_context.QuestItems.Count() == 0)
+            {
+                var quests = new List<QuestViewModel>
+                {
+                    new QuestViewModel { QuestID = Guid.NewGuid().ToString(), OwnerID = id, Type = "Daily" , Text = "Attend all Lectures", Accepted = true, Progress = 1, EndPoint = 3, EndDate = DateTime.Today.AddDays(30), Completed = false},
+                    new QuestViewModel { QuestID = Guid.NewGuid().ToString(), OwnerID = id, Type = "Quest", Text = "Complete Assignment 1", Accepted = true, Progress = 326, EndPoint = 1000, EndDate = DateTime.Now.AddHours(2), Completed = false},
+                    new QuestViewModel { QuestID = Guid.NewGuid().ToString(), OwnerID = id, Type = "Quest", Text = "Write 1000 Word Essay", Accepted = false, Progress = 0, EndPoint = 1000, EndDate = DateTime.Today.AddDays(7), Completed = false},
+                    new QuestViewModel { QuestID = Guid.NewGuid().ToString(), OwnerID = id, Type = "Quest", Text = "Select a Dissertation Topic", Accepted = true, Progress = 1, EndPoint = 1, EndDate = DateTime.Today.AddDays(2), Completed = true}
+                };
+                _context.AddRange(quests);
+            }
+
+            if (_context.AchievementItems.Count() == 0)
+            {
+                var achievements = new List<AchievementViewModel>
+                    {
+                        new AchievementViewModel { AchievementID = Guid.NewGuid().ToString(), OwnerID = id, Description = "Attend all lectures in a week", Completed = true, Current = 5, Goal = 5, AssociatedQuests = "Quest", PointsReward = 500 },
+                        new AchievementViewModel { AchievementID = Guid.NewGuid().ToString(), OwnerID = id, Description = "Complete 5 Assignments", Completed = false, Current = 4, Goal = 5, AssociatedQuests = "Quest", PointsReward = 500 },
+                        new AchievementViewModel { AchievementID = Guid.NewGuid().ToString(), OwnerID = id, Description = "Complete 10 Quests", Completed = false, Current = 8, Goal = 10, AssociatedQuests = "Quest", PointsReward = 500 },
+                        new AchievementViewModel { AchievementID = Guid.NewGuid().ToString(), OwnerID = id, Description = "Complete 3 Daily Quests", Completed = false, Current = 1, Goal = 3, AssociatedQuests = "Daily", PointsReward = 500 }
+                    };
+                _context.AddRange(achievements);
             }
 
             _context.SaveChanges();
